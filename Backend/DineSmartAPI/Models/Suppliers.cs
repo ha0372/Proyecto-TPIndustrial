ba39c2852f95c42e22da58c729e60e20a9818544
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2023-05-29 23:14:18 -0600

    Models

---
+++ Backend/DineSmartAPI/Models/Suppliers.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DineSmartAPI.Models
+{
+    [Table("Suppliers")]
+    public class Suppliers
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int supplier_id { get; set; }
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public string phone { get; set; }
+
+        public int email { get; set; }
+
+
+    }
+}
