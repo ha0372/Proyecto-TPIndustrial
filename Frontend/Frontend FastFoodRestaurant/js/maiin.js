1c28b3ae4f6333280c413875e36f0b6bee6f6aa7
Javier14 <69540480+JhavierQ14@users.noreply.github.com>
2023-06-01 17:34:39 -0600

    ArreglosVistas

--- Frontend/Frontend FastFoodRestaurant/js/maiin.js
+++ Frontend/Frontend FastFoodRestaurant/js/maiin.js
@@ -13,9 +13,47 @@ list.forEach((item) => item.addEventListener("mouseover", activeLink));
 // Menu Toggle
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
+let main = document.querySelector(".maiin");
 
 toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+   
+
+
+
+function loadPage(page) {
+  const contentDiv = document.getElementById('content');
+  fetch(`/${page}.html`)
+      .then(response => response.text())
+      .then(html => contentDiv.innerHTML = html)
+      .catch(error => console.warn(error));
+}
+function loadContent(page) {
+  const contentDiv = document.getElementById('content');
+  switch (page) {
+  
+    case 'ordenes':
+      loadPage('Ordenes')
+      break;
+    case 'ventas':
+      loadPage('Ventas')
+      break;
+    case 'compras':
+      loadPage('Compras')
+      break;
+    case 'inventario':
+      loadPage('Inventario')
+      break;
+    case 'empleados':
+      loadPage('EmpleadosUser')
+      break;
+    case 'historial':
+      loadPage('Historial')
+      break;
+    case 'configuracion':
+      loadPage('Configuraciones')
+      break;
+  }
+}
